c6d82e4ccb57cf6fbbdd687b4787502bbfa91bec
Stuart Taylor <stu@improbable.io>
2017-02-28 10:43:43 +0000

    add makeflags

--- tools/build_x86.sh
+++ tools/build_x86.sh
@@ -1,5 +1,6 @@
 export BUILD_CONFIG="Release"
 export BUILD_GENERATOR="Unix Makefiles"
 export BUILD_DEFINITIONS="-m32"
+export MAKEFLAGS="-j 8"
 git submodule update --init
 spatial build --log_level=debug
