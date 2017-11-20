19b466e31a9bf20ef6a6c739643c8dc4e9484fa1
Stuart Taylor <stu@improbable.io>
2017-11-20 12:04:24 +0000

    IP option

--- tools/local.sh
+++ tools/local.sh
@@ -3,5 +3,6 @@ set -e -u -x -o pipefail
 
 CONFIG=${CONFIG:-deploy/default.json}
 SNAPSHOT=${SNAPSHOT:-build/initial.snapshot}
+LOCAL_IP=${LOCAL_IP:-127.0.0.1}
 
-spatial local launch --snapshot=${SNAPSHOT} ${CONFIG}
+spatial local launch --runtime_ip=${LOCAL_IP} --snapshot=${SNAPSHOT} ${CONFIG}
